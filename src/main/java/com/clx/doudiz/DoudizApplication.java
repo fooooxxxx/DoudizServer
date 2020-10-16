8c27e0e68be91b077eb8abb8ab233a0ee2f1f089
lu <fooooxxxxlu@gmail.com>
2020-10-16 16:58:47 +0800

    create project

---
+++ src/main/java/com/clx/doudiz/DoudizApplication.java
@@ -0,0 +1,13 @@
+package com.clx.doudiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoudizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoudizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clx/doudiz/DoudizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clx.doudiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoudizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
